a257ba0aef3dc1d79b78aff93412614457276ad4
Elio Struyf <elio@struyfconsulting.be>
2024-03-20 17:52:41 +0100

    Added logging

--- Plugin/GitHubCopilotListenerPlugin.cs
+++ Plugin/GitHubCopilotListenerPlugin.cs
@@ -4,9 +4,22 @@ using System.Text.Json;
 
 namespace GitHub.Copilot.Listener.Plugin;
 
+public class GitHubCopilotListenerPluginConfiguration
+{
+  public string? LogPath { get; set; }
+}
+
 public class GitHubCopilotListenerPlugin : BaseProxyPlugin
 {
   public override string Name => nameof(GitHubCopilotListenerPlugin);
+
+  private readonly GitHubCopilotListenerPluginConfiguration _configuration = new();
+
+  public GitHubCopilotListenerPlugin()
+  {
+    Console.WriteLine("GitHub Copilot Listener Plugin Registered");
+    Console.WriteLine($"Runnin in: {Environment.CurrentDirectory}");
+  }
 
   public override void Register(IPluginEvents pluginEvents,
                                 IProxyContext context,
@@ -15,26 +28,39 @@ public class GitHubCopilotListenerPlugin : BaseProxyPlugin
   {
     base.Register(pluginEvents, context, urlsToWatch, configSection);
 
+    configSection?.Bind(_configuration);
+
     pluginEvents.BeforeRequest += BeforeRequest;
   }
 
   private Task BeforeRequest(object sender, ProxyRequestArgs e)
   {
+    var filePath = Path.Combine(Environment.CurrentDirectory, _configuration?.LogPath ?? "", $"copilot-{DateTime.Now.ToUniversalTime().ToString("yyyyMMdd-HHmm")}.txt");
+
+    _logger?.LogRequest(
+      [
+        filePath
+      ],
+      MessageType.Normal, new LoggingContext(e.Session)
+    );
+    File.WriteAllText(filePath, "Nothing to log");
+
     if (_urlsToWatch is null ||
       !e.HasRequestUrlMatch(_urlsToWatch) ||
       !e.Session.HttpClient.Request.Method.Equals("PATCH", StringComparison.CurrentCultureIgnoreCase))
     {
+      File.WriteAllText(filePath, "Nothing to log");
       return Task.CompletedTask;
     }
 
     try
     {
       var schemaString = e.Session.HttpClient.Request.BodyString;
-      Console.WriteLine(schemaString);
+      File.WriteAllText(filePath, schemaString);
     }
     catch (Exception ex)
     {
-      Console.WriteLine(ex.Message);
+      File.WriteAllText(filePath, ex.Message);
       return Task.CompletedTask;
     }
 
---
+++ README.md
@@ -0,0 +1,18 @@
+# Using the GitHub Copilot Listener
+
+## Build
+
+```bash
+dotnet build
+```
+
+## VSCode configuration
+
+Settings to update:
+
+```json
+{
+  "http.proxy": "http://localhost:8000",
+  "http.proxyStrictSSL": false,
+}
+```
