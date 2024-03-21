b780ab8508e89321339c47356c46b812fd4527b6
Elio Struyf <elio@struyfconsulting.be>
2024-03-21 10:31:35 +0100

    Plugin updates

--- Plugin/GitHubCopilotListenerPlugin.cs
+++ Plugin/GitHubCopilotListenerPlugin.cs
@@ -1,7 +1,7 @@
 using Microsoft.DevProxy.Abstractions;
 using Microsoft.Extensions.Configuration;
-using Titanium.Web.Proxy.Http;
 using System.Text.Json;
+using Titanium.Web.Proxy.EventArguments;
 
 namespace GitHub.Copilot.Listener.Plugin;
 
@@ -16,19 +16,28 @@ public class GitHubCopilotListenerPlugin : BaseProxyPlugin
 
   private readonly GitHubCopilotListenerPluginConfiguration _configuration = new();
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="GitHubCopilotListenerPlugin"/> class.
+  /// </summary>
   public GitHubCopilotListenerPlugin()
   {
     Console.WriteLine("GitHub Copilot Listener Plugin Registered");
-    Console.WriteLine($"Runnin in: {Environment.CurrentDirectory}");
+    Console.WriteLine($"Running in: {Environment.CurrentDirectory}");
   }
 
+  /// <summary>
+  /// Registers the plugin with the specified plugin events, context, URLs to watch, and configuration section.
+  /// </summary>
+  /// <param name="pluginEvents">The plugin events to register with.</param>
+  /// <param name="context">The proxy context.</param>
+  /// <param name="urlsToWatch">The set of URLs to watch.</param>
+  /// <param name="configSection">The configuration section.</param>
   public override void Register(IPluginEvents pluginEvents,
                                 IProxyContext context,
                                 ISet<UrlToWatch> urlsToWatch,
                                 IConfigurationSection? configSection = null)
   {
     base.Register(pluginEvents, context, urlsToWatch, configSection);
-    Console.WriteLine($"URLs: {urlsToWatch}");
 
     configSection?.Bind(_configuration);
 
@@ -36,48 +45,111 @@ public class GitHubCopilotListenerPlugin : BaseProxyPlugin
     pluginEvents.AfterResponse += AfterResponse;
   }
 
+  /// <summary>
+  /// Handles the BeforeRequest event.
+  /// </summary>
+  /// <param name="sender">The object that raised the event.</param>
+  /// <param name="e">The event arguments.</param>
+  /// <returns>A task representing the asynchronous operation.</returns>
   private Task BeforeRequest(object sender, ProxyRequestArgs e)
   {
-    var filePath = Path.Combine(Environment.CurrentDirectory, _configuration?.LogPath ?? "", $"copilot-{DateTime.Now.ToUniversalTime().ToString("yyyyMMdd-HHmm")}.txt");
-
-    _logger?.LogRequest(
-      [
-        filePath
-      ],
-      MessageType.Normal, new LoggingContext(e.Session)
-    );
-    File.WriteAllText(filePath, "Nothing to log");
-
     if (_urlsToWatch is null ||
       !e.HasRequestUrlMatch(_urlsToWatch))
     {
-      File.WriteAllText(filePath, "Nothing to log");
+      Console.WriteLine("No URL to watch.");
       return Task.CompletedTask;
     }
 
     try
     {
-      var schemaString = e.Session.HttpClient.Request.BodyString;
-      File.WriteAllText(filePath, schemaString);
+      var body = e.Session.HttpClient.Request.BodyString;
+
+      var uri = e.Session.HttpClient.Request.RequestUri;
+      if (uri?.AbsolutePath.Contains("/chat/completions") == true)
+      {
+        LogRequest(e.Session, body, "chat", "request");
+      }
+      else if (uri?.AbsolutePath.Contains("/copilot-codex/completions") == true)
+      {
+        LogRequest(e.Session, body, "inline", "request");
+      }
+      else
+      {
+        LogRequest(e.Session, body, "unknown", "request");
+      }
     }
     catch (Exception ex)
     {
-      File.WriteAllText(filePath, ex.Message);
+      LogRequest(e.Session, JsonSerializer.Serialize(ex.Message), "error", "request");
       return Task.CompletedTask;
     }
 
     return Task.CompletedTask;
   }
 
+  /// <summary>
+  /// Represents an asynchronous operation that can return a value.
+  /// </summary>
+  /// <returns>A task that represents the asynchronous operation.</returns>
   private Task AfterResponse(object? sender, ProxyResponseArgs e)
   {
-    Request request = e.Session.HttpClient.Request;
-    if (_urlsToWatch is not null &&
-        e.HasRequestUrlMatch(_urlsToWatch))
+    if (_urlsToWatch is null ||
+      !e.HasRequestUrlMatch(_urlsToWatch))
     {
-
+      Console.WriteLine("No URL to watch.");
+      return Task.CompletedTask;
     }
-    Console.WriteLine("AfterResponse");
+
+    try
+    {
+      var body = e.Session.HttpClient.Response.BodyString;
+
+      var uri = e.Session.HttpClient.Request.RequestUri;
+      if (uri?.AbsolutePath.Contains("/chat/completions") == true)
+      {
+        LogRequest(e.Session, body, "chat", "response");
+      }
+      else if (uri?.AbsolutePath.Contains("/copilot-codex/completions") == true)
+      {
+        LogRequest(e.Session, body, "inline", "response");
+      }
+      else
+      {
+        LogRequest(e.Session, body, "unknown", "response");
+      }
+    }
+    catch (Exception ex)
+    {
+      LogRequest(e.Session, JsonSerializer.Serialize(ex.Message), "error", "response");
+      return Task.CompletedTask;
+    }
+
     return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// Logs the request information to a file.
+  /// </summary>
+  /// <param name="session">The session event arguments.</param>
+  /// <param name="schemaString">The schema string to be logged.</param>
+  /// <param name="request">The request string to be logged.</param>
+  /// <param name="type">The type of the request.</param>
+  private void LogRequest(SessionEventArgs session, string schemaString, string request, string type)
+  {
+    try
+    {
+      var filePath = Path.Combine(
+        Environment.CurrentDirectory,
+        _configuration?.LogPath ?? "",
+        type,
+        $"copilot-{request}-{session.ClientConnectionId}.json");
+      var file = new FileInfo(filePath);
+      file.Directory?.Create();
+      File.WriteAllText(file.FullName, schemaString);
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.Message);
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Using the GitHub Copilot Listener
 
-## Build
+The GitHub Copilot Listener plugin listens to the GitHub Copilot requests and logs them to a file.
+
+![GitHub Copilot Listener](./assets/github-copilot-listener.png)
+
+## Prerequisites
+
+- [.NET Core SDK](https://dotnet.microsoft.com/download)
+- [Dev Proxy](https://learn.microsoft.com/en-us/microsoft-cloud/dev/dev-proxy/overview)
+
+## Build the plugin
 
 ```bash
 dotnet build
@@ -8,11 +17,50 @@ dotnet build
 
 ## VSCode configuration
 
-Settings to update:
+To listen to the GitHub Copilot requests in Visual Studio Code, the Dev Proxy must be running and the following settings must be set in the VSCode **user settings**:
 
 ```json
 {
   "http.proxy": "http://localhost:8000",
   "http.proxyStrictSSL": false,
 }
+```
+
+## Dev Proxy Configuration
+
+Use the following configuration for the plugin:
+
+### Plugin instance definition
+
+```json
+{
+  "name": "GitHubCopilotListenerPlugin",
+  "enabled": true,
+  "pluginPath": "./bin/Debug/net8.0/github-copilot-calls.dll",
+  "configSection": "githubCopilotListener"
+}
+```
+
+### Configuration example
+
+```json
+{
+  "githubCopilotListener": {
+    "logPath": "./logs"
+  }
+}
+```
+
+### Configuration properties
+
+| Property | Description | Default |
+|----------|-------------|--------:|
+| `logPath` | The path to which the logs need to be written | `""` |
+
+## Run the Dev Proxy with the plugin
+
+Once you have created your configuration, you can run the Dev Proxy with the plugin as follows:
+
+```bash
+devproxy
 ```
